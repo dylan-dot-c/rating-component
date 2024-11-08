ab3f641168b9dbcc9c206217415fcba7e14f72cb
Dylan Heslop <heslopd23@gmail.com>
2024-11-08 12:41:39 -0500

    added validation

--- static/form.js
+++ static/form.js
@@ -1,8 +1,9 @@
 const ratingButtons = document.querySelectorAll(".rating-button");
 const hiddenInput = document.getElementById("rating");
+const form = document.getElementById("form");
 
 ratingButtons.forEach((btn) => {
-    btn.addEventListener("click", (event) => {
+    btn.addEventListener("click", () => {
         ratingButtons.forEach((btn) => {
             btn.setAttribute("aria-pressed", false);
             btn.classList.remove("selected");
@@ -12,4 +13,11 @@ ratingButtons.forEach((btn) => {
 
         hiddenInput.value = btn.getAttribute("data-value");
     });
+});
+
+form.addEventListener("submit", (e) => {
+    if (hiddenInput.value == "") {
+        alert("Please select a value");
+        e.preventDefault();
+    }
 });
